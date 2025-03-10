2943feec716bc226a43b1821059a2baf5c5d1eb5
Kwon-Kiseok <soorison@naver.com>
2025-03-10 14:26:07 +0900

    [기능] 탄약 부족 경고 UI 추가

--- Source/projectKTY/Private/UI/HUD/PlayerHUDWidget.cpp
+++ Source/projectKTY/Private/UI/HUD/PlayerHUDWidget.cpp
@@ -8,9 +8,19 @@ void UPlayerHUDWidget::NativeConstruct()
 {
 	Super::NativeConstruct();
 
+	if (Crosshair)
+	{
+		Crosshair->SetVisibility(ESlateVisibility::Visible);
+	}
+
 	if (!AmmoText)
 	{
 		UE_LOG(LogTemp, Error, TEXT("AmmoText Bind Failed"));
+	}
+
+	if (AmmoWarningText)
+	{
+		AmmoWarningText->SetVisibility(ESlateVisibility::Hidden);
 	}
 }
 
@@ -26,5 +36,22 @@ void UPlayerHUDWidget::UpdateAmmoDisplay(int32 CurrentAmmo, int32 Magazine)
 	else
 	{
 		UE_LOG(LogTemp, Error, TEXT("AmmoText Update Failed"));
+	}
+
+	if (AmmoWarningText)
+	{
+		if (CurrentAmmo == 0)
+		{
+			AmmoWarningText->SetVisibility(ESlateVisibility::Visible);
+			Crosshair->SetVisibility(ESlateVisibility::Hidden);
+		}
+		else
+		{
+			AmmoWarningText->SetVisibility(ESlateVisibility::Hidden);
+			if (Crosshair->GetVisibility() == ESlateVisibility::Hidden)
+			{
+				Crosshair->SetVisibility(ESlateVisibility::Visible);
+			}
+		}
 	}
 }
